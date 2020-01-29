69ad820f5a38d3ba3166c10db4e0856fa1a9bbc1
eriq2207 <eriq2207@gmail.com>
2020-01-29 13:16:44 +0100

    Front-end finished

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const http = require('http').createServer(app)
 const path = require('path')
+const io = require('socket.io')(http)
 
 app.get('/', (req,res )=> {
     res.sendFile(path.join(__dirname,'/chat.html'))
 })
 app.listen(80,()=> console.log("Serwer uruchomiony na porcie 80"))
+
+io.on('connection', (socket)=>{
+    console.log("User connected")
+})
+io.on('disconnect', (socket) =>{
+    console.log("User disconnected")
+})
