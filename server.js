f31b50463a56fed4ca51ceb8e2eea6cac3e0fbf8
eriq2207 <eriq2207@gmail.com>
2020-01-30 11:44:59 +0100

    Added login

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
+var chat_users =[];
 //Send static files
 app.use(express.static(__dirname + '/static'));
 
 //HTTP Server
 const port = process.env.PORT || 3000;
-var server = app.listen(port,()=> console.log("Serwer uruchomiony na porcie 80"));
+var server = app.listen(port,()=> console.log("Serwer uruchomiony na porcie:" + port));
 app.get('/', (req,res )=> {
     res.sendFile(path.join(__dirname,'/chat.html'));
 })
@@ -15,13 +17,28 @@ app.get('/', (req,res )=> {
 //Socket io
 var io = require('socket.io').listen(server);
 io.on('connection', function(user){
-    console.log('a user connected');
-    user.on('disconnect', function(){
-        console.log("User disconnected!");
-    })
-    user.on('chat message', function(msg){
-        io.emit('chat message', msg);
-      });
-})
 
+    user.on('disconnect', function(){
+            
+    })
+
+    user.on('login data', function(msg){
+        var chat_user = {
+            id:'',
+            login:''
+        }
+        chat_user.id = user.id;
+        chat_user.login = msg;
+        chat_users.push(chat_user);
+        user.emit('login status','1');
+    });
+    user.on('chat message', function(msg){
+        var result = chat_users.filter(obj =>{
+            return obj.id == user.id;
+        })
+        if(result.length===1)
+            io.emit('chat message',result[0].login + ': ' + msg);
+
+    });
+})
 
--- static/js/socket_client.js
+++ static/js/socket_client.js
@@ -1,21 +1,63 @@
 function socket_init()
 {
+    //init socket io
     var socket = io();
+    //Send message with Enter pressed
     $('#send_textbox').keypress(function(e){
         if(e.which == 13 && !e.shiftKey) {        
             $('#send_form').submit();
             e.preventDefault();
         }
     });
+    //Send login with Enter pressed
+    $('#send_login_textbox').keypress(function(e){
+      if(e.which == 13 && !e.shiftKey) {        
+          $('#send_login_form').submit();
+          e.preventDefault();
+      }});
+    //Sending login data
+    $('#send_login_form').submit(function(e){
+      e.preventDefault();
+      socket.emit('login data', $('#send_login_textbox').val());
+    });
+    //Receiving login status information
+    socket.on('login status',function(msg){
+      if(msg=='1')
+      {
+        removeElement('login_div');
+        addElement('chat_div','ul','chat_list','');
+        $('#send_textbox').prop('disabled',false);
+        $('#send_btn').prop('disabled',false);
+      }
+      else
+      {
+
+      }
+    });
+    //Sending message to server
     $('#send_form').submit(function(e){
       e.preventDefault(); // prevents page reloading
       socket.emit('chat message', $('#send_textbox').val());
       $('#send_textbox').val('');
       return false;
     });
+    //Receiving message from server
     socket.on('chat message', function(msg){
-        $('#chat_list_id').append($('<li>').text(msg));
-        var list_obj = document.getElementById("chat_list_id");
+        $('#chat_list').append($('<li>').text(msg));
+        var list_obj = document.getElementById("chat_list");
         list_obj.scrollTop = list_obj.scrollHeight;
-      });
+    });
+}
+function addElement(parentId, elementTag, elementId, html) {
+  // Adds an element to the document
+  var p = document.getElementById(parentId);
+  var newElement = document.createElement(elementTag);
+  newElement.setAttribute('id', elementId);
+  newElement.innerHTML = html;
+  p.appendChild(newElement);
+}
+function removeElement(elementId) {
+  // Removes an element from the document
+  var element = document.getElementById(elementId);
+  element.parentNode.removeChild(element);
 }
