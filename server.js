6e8938ac07087b169216a9cbd497640a377716b3
eriq2207 <eriq2207@gmail.com>
2020-01-31 10:40:47 +0100

    Added users and checking connection

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-
-var chat_users =[];
+//Temp users
+var chat_user1 = {
+    id:'',
+    login:'Eryk',
+    password:'1234'
+}
+var chat_user2 = {
+    id:'',
+    login:'Maciek',
+    password:'1234'
+}
+var actual_chat_users =[];
+var chat_all_users = [];
+chat_all_users.push(chat_user1);
+chat_all_users.push(chat_user2);
 //Send static files
 app.use(express.static(__dirname + '/static'));
 
@@ -19,36 +32,54 @@ var io = require('socket.io').listen(server);
 io.on('connection', function(user){
 
     user.on('disconnect', function(){
-        var index = chat_users.findIndex(obj => obj.id === user.id);
-        chat_users.splice(index,1);
-        //implement actual users list
+        var index = actual_chat_users.findIndex(obj => obj.id === user.id);
+        actual_chat_users.splice(index,1);
+        update_users(actual_chat_users,io, actual_chat_users);
     })
 
-    user.on('login data', function(msg){
+    user.on('login request', function(msg){
         var chat_user = {
             id:'',
-            login:''
+            login:'',
+            password:''
         }
+        var login_data = msg.split(';',2);
+
         chat_user.id = user.id;
-        chat_user.login = msg;
-        var result = chat_users.findIndex(obj => obj.login === chat_user.login);
-        if(result==-1)
+        chat_user.login = login_data[0];
+        chat_user.password =login_data[1];
+
+        var result = chat_all_users.findIndex(obj => obj.login == chat_user.login);
+
+        if(chat_all_users[result].password === chat_user.password)
         {
-            chat_users.push(chat_user);
-            user.emit('login status','1');
+            actual_chat_users.push(chat_user);
+            user.emit('login request status','1');
+            update_users(actual_chat_users,io, actual_chat_users);
         }
         else
         {
-            user.emit('login status','0');
+            user.emit('login request status','0');
         }
     });
     user.on('chat message', function(msg){
-        var result = chat_users.filter(obj =>{
+        var result = actual_chat_users.filter(obj =>{
             return obj.id == user.id;
         })
         if(result.length===1)
-            io.emit('chat message',result[0].login + ': ' + msg);
+        {  
+            for(i=0;i<actual_chat_users.length;i++)
+                io.sockets.connected[actual_chat_users[i].id].emit('chat message',result[0].login + ': ' + msg);
+        }
 
     });
 })
-
+function update_users(actual_users, io, actual_users)
+{
+    //Take login table from table of objects
+    var users_login = actual_users.map(a =>a.login);
+    var msg =users_login.join(';');
+    
+    for(var i=0;i<actual_chat_users.length;i++)
+        io.sockets.connected[actual_chat_users[i].id].emit('users update',msg +';');
+}
--- static/js/socket_client.js
+++ static/js/socket_client.js
@@ -3,6 +3,7 @@ function socket_init()
     var login='';
     //init socket io
     var socket = io();
+    setInterval(check_connection,1000,socket);
     //Send message with Enter pressed
     $('#send_textbox').keypress(function(e){
         if(e.which == 13 && !e.shiftKey) {        
@@ -19,11 +20,11 @@ function socket_init()
     //Sending login data
     $('#send_login_form').submit(function(e){
       e.preventDefault();
-      socket.emit('login data', $('#send_login_textbox').val());
+      socket.emit('login request', $('#send_login_textbox').val()+';'+$('#send_password_textbox').val());
       login = $('#send_login_textbox').val();
     });
     //Receiving login status information
-    socket.on('login status',function(msg){
+    socket.on('login request status',function(msg){
       if(msg=='1')
       {
         removeElement('login_div');
@@ -37,7 +38,7 @@ function socket_init()
           login ='';
           if(!$('#warning_id').length)
           {
-            var warning = $('<p1>').text("Istnieje użytkownik o takim nicku");
+            var warning = $('<p1>').text("Błędna nazwa użytkownika lub hasło");
             warning.attr('id','warning_id');
             warning.css('color', 'red');
             $('#login_div').append(warning);
@@ -47,13 +48,15 @@ function socket_init()
     //Sending message to server
     $('#send_form').submit(function(e){
       e.preventDefault(); // prevents page reloading
-      socket.emit('chat message', $('#send_textbox').val());
+      var msg = $('#send_textbox').val();
+      if(msg!='')
+        socket.emit('chat message', $('#send_textbox').val());
       $('#send_textbox').val('');
       return false;
     });
     //Receiving message from server
     socket.on('chat message', function(msg){
-
+        console.log("Otrzymalem wiadomosc");
         var el = $('<li>').text(msg);
 
         var received_login = msg.split(':',1);
@@ -69,6 +72,17 @@ function socket_init()
         var list_obj = document.getElementById("chat_list");
         list_obj.scrollTop = list_obj.scrollHeight;
     });
+    //Updating user list
+    socket.on('users update', function(msg){
+      var users = msg.split(';',200);
+      var users_list = $('#users_list');
+      users_list.empty();
+      for(i=0;i<users.length;i++)
+      {
+        var el = $('<li>').text(users[i]);
+        users_list.append(el);
+      }
+  });
 }
 function addElement(parentId, elementTag, elementId, html) {
   // Adds an element to the document
@@ -82,4 +96,15 @@ function removeElement(elementId) {
   // Removes an element from the document
   var element = document.getElementById(elementId);
   element.parentNode.removeChild(element);
+}
+function check_connection(socket){
+  
+  if(!socket.connected && !($('#con_err_msg').length))
+  {
+    var el = $('<li>').text("Problem z połączeniem z serwerem.. odśwież strone i zaloguj się ponownie");
+    el.css('color', 'red');
+    el.css('text-align','center');
+    el.attr('id','con_err_msg');
+    $('#chat_list').append(el);
+  } 
 }
