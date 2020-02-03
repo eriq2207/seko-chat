135705ef711689c0ada745e917a794b7689e8625
eriq2207 <eriq2207@gmail.com>
2020-02-03 13:23:06 +0100

    Added login users and connection to database

--- server.js
+++ server.js
@@ -5,55 +5,51 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt');
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended : true}));
+const saltRounds = 10;
+var actual_chat_users = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/static'));
 
-var actual_chat_users =[];
-
 //HTTP Server
 const port = process.env.PORT || 3000;
-var server = app.listen(port,()=> console.log("Serwer uruchomiony na porcie:" + port));
-app.get('/', (req,res )=> {
-    res.sendFile(path.join(__dirname,'/chat.html'));
+var server = app.listen(port, () => console.log("Serwer uruchomiony na porcie:" + port));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/chat.html'));
 })
-app.post('/login',(req,res) =>{
+app.post('/login', (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
     var id = req.body.id;
-    bcrypt.hash(password,10,(err,hash) =>{
-        if(err) throw err;
-        var sql = "SELECT 'password' FROM seko_chat_users WHERE login = '"+username+"'";
-        if(con.state === 'authenticated')
-        {
-            con.query(sql,(err,result) =>{
-                if(err) throw err;
-                if(result.length === 1)
-                {
-                    bcrypt.compare(result[0].password, hash, function(err, res) {
-                        if (err) throw err;
-                        if(res)
-                        {
-                            var user={
-                                id: req.body.id,
-                                login: req.body.username,
-                            }
-                            io.sockets.connected[id].emit('login request status','1');
-                            actual_chat_users.push(user);
+    var sql = "SELECT * FROM seko_chat_users WHERE login = '" + username + "'";
+    if (con.state === 'authenticated') {
+        con.query(sql, (err, result) => {
+            if (err) throw err;
+            if (result.length === 1 && username===result[0].login) {
+                bcrypt.compare(password, result[0].password, function (err, res) {
+                    if (err) throw err;
+                    if (res) {
+                        var user = {
+                            id: req.body.id,
+                            login: req.body.username,
                         }
-                        else
-                        {
-                            io.sockets.connected[id].emit('login request status','0');
-                        }
-                    })
-                }
-            })
-        }
-        else
-        {
-            io.sockets.connected[id].emit('login request status','-1');
-        } 
-    })
+                        io.sockets.connected[id].emit('login request status', '1');
+                        actual_chat_users.push(user);
+                        update_users(actual_chat_users,io, user.id);
+                        add_new_user(actual_chat_users,io, user.login, user.id);
+                    }
+                    else 
+                        io.sockets.connected[id].emit('login request status', '0');
+                })
+            }
+            else
+                io.sockets.connected[id].emit('login request status', '0');
+        })
+    }
+    else {
+        io.sockets.connected[id].emit('login request status', '-1');
+    }
 })
 
 
@@ -65,85 +61,54 @@ var con = mysql.createConnection({
     password: 'HnCxMUYeT7',
     database: 'fmMb3PtZox',
 })
-con.connect((err) =>{
-    if(err) throw err;
+con.connect((err) => {
+    if (err) throw err;
     console.log("Połączono do bazy danych");
 })
 //Socket io
 var io = require('socket.io').listen(server);
-io.on('connection', function(user){
+io.on('connection', function (user) {
 
-    user.on('disconnect', function(){
+    user.on('disconnect', function () {
         var index = actual_chat_users.findIndex(obj => obj.id === user.id);
-        actual_chat_users.splice(index,1);
-        update_users(actual_chat_users,io, actual_chat_users);
+        if(index >=0)
+        {
+            var login = actual_chat_users[index].login;
+            actual_chat_users.splice(index, 1);
+            delete_user(actual_chat_users, io, login);
+        }
     })
 
-    user.on('login request', function(msg){
-        var chat_user = {
-            id:'',
-            login:'',
-            password_hash:''
-        }
-        var login_data = msg.split(';',2);
-
-        chat_user.id = user.id;
-        chat_user.login = login_data[0];
-        bcrypt.hash(login_data[1],10,(err,hash) =>{
-            if(err) throw err;
-            chat_user.password_hash = hash;
-        });
-        var sql = '';
-        if(con.connected)
-        {
-            con.query(sql,(err,result) =>{
-                if(err) throw err;
-                if(result.length === 1)
-                {
-
-                }
-                else
-                {
-                    user.emit('login request status','0');
-                }
-            })
-        }
-        else
-        {
-            user.emit('login request status','-1');
-        } 
-        
-        var result = chat_all_users.findIndex(obj => obj.login == chat_user.login);
-
-        if(chat_all_users[result].password === chat_user.password)
-        {
-            actual_chat_users.push(chat_user);
-            user.emit('login request status','1');
-            update_users(actual_chat_users,io, actual_chat_users);
-        }
-        else
-        {
-            user.emit('login request status','0');
-        }
-    });
-    user.on('chat message', function(msg){
-        var result = actual_chat_users.filter(obj =>{
+    user.on('chat message', function (msg) {
+        var result = actual_chat_users.filter(obj => {
             return obj.id == user.id;
         })
-        if(result.length===1)
-        {  
-            for(i=0;i<actual_chat_users.length;i++)
-                io.sockets.connected[actual_chat_users[i].id].emit('chat message',result[0].login + ': ' + msg);
+        if (result.length === 1) {
+            for (i = 0; i < actual_chat_users.length; i++)
+                io.sockets.connected[actual_chat_users[i].id].emit('chat message', result[0].login + ': ' + msg);
         }
 
     });
 })
-function update_users(actual_users, io, actual_users)
-{
+function update_users(actual_users, io, user_to_update_id) {
     //Take login table from table of objects
-    var users_login = actual_users.map(a =>a.login);
-    var msg =users_login.join(';');
-
-    for(var i=0;i<actual_chat_users.length;i++)
-        io.sockets.connected[actual_chat_users[i].id].emit('users update',msg +';');
+    var users_login = actual_users.map(a => a.login);
+    var msg = users_login.join(';');
+    io.sockets.connected[user_to_update_id].emit('users update', msg + ';');
+}
+function add_new_user(actual_users, io, user_add, user_ignore_id) {
+    for (var i = 0; i < actual_chat_users.length; i++)
+    {
+        var socket = io.sockets.connected[actual_users[i].id];
+        if(socket!=null && socket.id!=user_ignore_id)
+            socket.emit('user add', user_add);
+    }
+}
+function delete_user(actual_users, io, user_delete) {
+    for (var i = 0; i < actual_chat_users.length; i++)
+    {
+        var socket = io.sockets.connected[actual_users[i].id];
+        if(socket!=null)
+            socket.emit('user delete', user_delete);
+    }
 }
--- static/js/socket_client.js
+++ static/js/socket_client.js
@@ -1,96 +1,130 @@
-function socket_init()
-{
-    var login='';
-    //init socket io
-    var socket = io();
-    setInterval(check_connection,1000,socket);
-    //Send message with Enter pressed
-    $('#send_textbox').keypress(function(e){
-        if(e.which == 13 && !e.shiftKey) {        
-            $('#send_form').submit();
-            e.preventDefault();
-        }
-    });
-    //Send login with Enter pressed
-    $('#send_login_textbox').keypress(function(e){
-      if(e.which == 13 && !e.shiftKey) {        
-          $('#send_login_form').submit();
-          e.preventDefault();
-      }});
-    //Sending login data
-    $('#send_login_form').submit(function(e){
+function socket_req_handling() {
+  var login = '';
+  //init socket io
+  var socket = io();
+  setInterval(check_connection, 1000, socket);
+  //Send message with Enter pressed
+  $('#send_textbox').keypress(function (e) {
+    if (e.which == 13 && !e.shiftKey) {
+      $('#send_form').submit();
       e.preventDefault();
-      $.post("login",{
-        username: $('#send_login_textbox').val(),
-        password: $('#send_password_textbox').val(),
-        id: socket.id,
-      })
-      //socket.emit('login request', $('#send_login_textbox').val()+';'+$('#send_password_textbox').val());
-      //login = $('#send_login_textbox').val();
-    });
-    //Get socket session ID
-    socket.on('connect',()=>{
-      socket_session_id = socket.id;
+    }
+  });
+  //Send login with Enter pressed
+  $('#send_login_textbox').keypress(function (e) {
+    if (e.which == 13 && !e.shiftKey) {
+      $('#send_login_form').submit();
+      e.preventDefault();
+    }
+  });
+  //Sending login data
+  $('#send_login_form').submit(function (e) {
+    e.preventDefault();
+    $.post("login", {
+      username: $('#send_login_textbox').val(),
+      password: $('#send_password_textbox').val(),
+      id: socket.id,
     })
-    //Receiving login status information
-    socket.on('login request status',function(msg){
-      if(msg=='1')
-      {
-        removeElement('login_div');
-        addElement('chat_div','ul','chat_list','');
-        $('#send_textbox').prop('disabled',false);
-        $('#send_btn').prop('disabled',false);
-        $('#send_textbox').focus();
-      }
-      else
-      {
-          login ='';
-          if(!$('#warning_id').length)
-          {
+  });
+  //Receiving login status information
+  socket.on('login request status', function (msg) {
+    switch (msg) {
+      case '1':
+        {
+          login = $('#send_login_textbox').val();
+          removeElement('login_div');
+          addElement('chat_div', 'ul', 'chat_list', '');
+          $('#send_textbox').prop('disabled', false);
+          $('#send_btn').prop('disabled', false);
+          $('#send_textbox').focus();
+        }
+      case '0':
+        {
+          if (!$('#warning_id').length) {
             var warning = $('<p1>').text("Błędna nazwa użytkownika lub hasło");
-            warning.attr('id','warning_id');
+            warning.attr('id', 'warning_id');
             warning.css('color', 'red');
             $('#login_div').append(warning);
           }
-      }
-    });
-    //Sending message to server
-    $('#send_form').submit(function(e){
-      e.preventDefault(); // prevents page reloading
-      var msg = $('#send_textbox').val();
-      if(msg!='')
-        socket.emit('chat message', $('#send_textbox').val());
-      $('#send_textbox').val('');
-      return false;
-    });
-    //Receiving message from server
-    socket.on('chat message', function(msg){
-        console.log("Otrzymalem wiadomosc");
-        var el = $('<li>').text(msg);
-
-        var received_login = msg.split(':',1);
-        if(received_login==login)
-        {
-          el.css('color', 'white');
-          el.css('text-align','right');
-          el.css('padding-right','3%');
         }
+      case '-1':
+        {
+          var warning = $('<p1>').text("Problem z połączeniem z bazą danych. Skontaktuj się z administratorem.");
+          warning.attr('id', 'warning_id');
+          warning.css('color', 'red');
+          if (!$('#warning_id').length) {
+            $('#login_div').append(warning);
+          }
+          else {
+            $('#warning_id') = warning;
+          }
+        }
+    }
 
-        $('#chat_list').append(el);
+  });
+  //Sending message to server
+  $('#send_form').submit(function (e) {
+    e.preventDefault(); // prevents page reloading
+    var msg = $('#send_textbox').val();
+    if (msg != '')
+      socket.emit('chat message', $('#send_textbox').val());
+    $('#send_textbox').val('');
+    return false;
+  });
+  //Receiving message from server
+  socket.on('chat message', function (msg) {
+    console.log("Otrzymalem wiadomosc");
+    var el = $('<li>').text(msg);
 
-        var list_obj = document.getElementById("chat_list");
-        list_obj.scrollTop = list_obj.scrollHeight;
-    });
-    //Updating user list
-    socket.on('users update', function(msg){
-      var users = msg.split(';',200);
-      var users_list = $('#users_list');
-      users_list.empty();
-      for(i=0;i<users.length;i++)
+    var received_login = msg.split(':', 1);
+    if (received_login == login) {
+      el.css('color', 'white');
+      el.css('text-align', 'right');
+      el.css('padding-right', '3%');
+    }
+
+    $('#chat_list').append(el);
+
+    var list_obj = document.getElementById("chat_list");
+    list_obj.scrollTop = list_obj.scrollHeight;
+  });
+  //Update users list
+  socket.on('users update', function (msg) {
+    var users = msg.split(';', 200);
+    var users_list = $('#users_list');
+    users_list.empty();
+    for (i = 0; i < users.length -1; i++) {
+      var el = $('<li>').text(users[i]);
+      users_list.append(el);
+    }
+  });
+  socket.on('user add', function (msg) {
+    var chat_msg = $('<li>').text(msg + ' dołączył/a do czatu');
+    var user_msg = $('<li>').text(msg);
+    var user_list = $('#users_list');
+    var chat_list = $('#chat_list');
+
+    chat_msg.css('color', 'blue');
+
+    user_list.append(user_msg);
+    chat_list.append(chat_msg);
+  });
+  socket.on('user delete', function (msg) {
+    var chat_msg = $('<li>').text(msg + ' opuścił/a czat');
+    var chat_list = $('#chat_list');
+    var user_list = $('#users_list');
+    chat_msg.css('color', 'blue');
+    chat_list.append(chat_msg);
+    var childs = user_list.children();
+    for(var i = 0;i<childs.length;i++)
+    {
+      var child = childs[i];
+      if(child.textContent === msg)
       {
-        var el = $('<li>').text(users[i]);
-        users_list.append(el);
+        child.remove();
+        break;
       }
+    }
   });
 }
 function addElement(parentId, elementTag, elementId, html) {
@@ -106,14 +140,13 @@ function removeElement(elementId) {
   var element = document.getElementById(elementId);
   element.parentNode.removeChild(element);
 }
-function check_connection(socket){
-  
-  if(!socket.connected && !($('#con_err_msg').length))
-  {
+function check_connection(socket) {
+
+  if (!socket.connected && !($('#con_err_msg').length)) {
     var el = $('<li>').text("Problem z połączeniem z serwerem.. odśwież strone i zaloguj się ponownie");
     el.css('color', 'red');
-    el.css('text-align','center');
-    el.attr('id','con_err_msg');
+    el.css('text-align', 'center');
+    el.attr('id', 'con_err_msg');
     $('#chat_list').append(el);
-  } 
+  }
 }
